5fd6c70808177f3313093c39ddd8f86bd75827a3
George Alex Xenos <74962262+GeorgeAlexXenos@users.noreply.github.com>
2021-12-07 14:45:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,26 +2,20 @@ function validEmail() {
   let mail = document.querySelector(".mail");
   let email = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
   let errorMessage = document.querySelector("span.error-message");
-
-  var slidingTagLiAfterStyle = document.createElement("style");
-  slidingTagLiAfterStyle.innerHTML = `button:before {
-  content: url(/images/icon-error.svg);
-  position: absolute;
-  display: block;
-}`;
+  let errorIcon = document.querySelector(".error-icon");
 
   if (mail.value.match(email)) {
     alert("Your Email : " + mail.value);
   } else {
     function displayErrorMessage() {
       event.preventDefault();
-      document.head.appendChild(slidingTagLiAfterStyle);
       errorMessage.style.display = "block";
+      errorIcon.style.display = "block";
     }
     displayErrorMessage();
     setTimeout(() => {
-      document.head.removeChild(slidingTagLiAfterStyle);
       errorMessage.style.display = "none";
+      errorIcon.style.display = "none";
     }, 2500);
   }
 }
