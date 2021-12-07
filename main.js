dbfa30f3008a05ed913fd8703a587b29f979df56
georgealex <georgealex1982@gmail.com>
2021-12-07 13:54:55 +0200

    fix error icon

--- main.js
+++ main.js
@@ -2,16 +2,41 @@ function validEmail() {
   let mail = document.querySelector(".mail");
   let email = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
   let errorMessage = document.querySelector("span.error-message");
+
+  var slidingTagLiAfterStyle = document.createElement("style");
+  slidingTagLiAfterStyle.innerHTML = `button:before {
+  content: url(/images/icon-error.svg);
+  position: absolute;
+  right: 80px;
+  display: "block";
+}`;
+
   if (mail.value.match(email)) {
     alert("Your Email : " + mail.value);
   } else {
     function displayErrorMessage() {
-      errorMessage.style.display = "block";
       event.preventDefault();
+      document.head.appendChild(slidingTagLiAfterStyle);
+      errorMessage.style.display = "block";
     }
     displayErrorMessage();
     setTimeout(() => {
-      return (errorMessage.style.display = "none");
+      document.head.removeChild(slidingTagLiAfterStyle);
+      errorMessage.style.display = "none";
     }, 2500);
   }
 }
+
+// let errorIconMessage = document.querySelector("button"), "::before";
+// errorIconMessage.style.display = "block";
+// console.log(errorIconMessage);
+
+// let displayBeforeIcon = window
+//   .getComputedStyle(document.querySelector("button"), ":before")
+//   .setPropertyValue("display");
+// displayBeforeIcon.setPropertyValue("display")= "block";
+
+// content: url(/images/icon-error.svg);
+//   position: absolute;
+//   right: 80px;
+//   display: none;
